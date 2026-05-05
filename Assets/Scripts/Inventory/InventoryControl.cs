49bea9cfd8cd0922e100c9adead70ede6f452b01
Melodeitea <118695764+Melodeitea@users.noreply.github.com>
2026-05-05 18:46:11 +0200

    can close

--- Assets/Scripts/Inventory/InventoryControl.cs
+++ Assets/Scripts/Inventory/InventoryControl.cs
@@ -7,17 +7,29 @@ public class InventoryControl : MonoBehaviour
 	public GameObject inventoryScreen;
 	public GameObject inventoryFade;
 	public AudioSource inventoryOpen;
+	public bool isOpen = false;
+	public AudioSource inventoryClose;
+	public bool canClose = false;
 
-	
+
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Tab))
+		if (Input.GetKeyDown(KeyCode.Tab) && isOpen == false && canClose == false)
 		{
-			Debug.Log("Tab key was pressed.");
-			
+			// Debug.Log("Tab key was pressed.");
+
+			isOpen = true;
 			inventoryOpen.Play();
 			inventoryFade.SetActive(true);
 			StartCoroutine(InvOpen());
+		}
+
+		if (Input.GetKeyDown(KeyCode.Tab) && isOpen == true && canClose == true)
+		{
+			isOpen = false;
+			inventoryClose.Play();
+			inventoryFade.SetActive(true);
+			StartCoroutine(InvClose());
 		}
 	}
 
@@ -27,6 +39,17 @@ public class InventoryControl : MonoBehaviour
 		inventoryScreen.SetActive(true);
 		yield return new WaitForSeconds(0.25f);
 		inventoryFade.SetActive(false);
+		yield return new WaitForSeconds(0.5f);
+		canClose = true;
 	}
 
+	IEnumerator InvClose()
+	{
+		yield return new WaitForSeconds(0.25f);
+		inventoryScreen.SetActive(false);
+		yield return new WaitForSeconds(0.25f);
+		inventoryFade.SetActive(false);
+		yield return new WaitForSeconds(0.5f);
+		canClose = false;
+	}
 }
