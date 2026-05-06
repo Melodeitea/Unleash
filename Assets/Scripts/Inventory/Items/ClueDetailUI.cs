a383071f3b9534a948adf62e97662b7e4cac05b9
Melodeitea <118695764+Melodeitea@users.noreply.github.com>
2026-05-06 18:21:11 +0200

    clues workingggggg
    ui bad af tho
    gotta check when song is reset, maybe too late

--- Assets/Scripts/Inventory/Items/ClueDetailUI.cs
+++ Assets/Scripts/Inventory/Items/ClueDetailUI.cs
@@ -7,15 +7,27 @@ public class ClueDetailUI : MonoBehaviour
 	[SerializeField] private TextMeshProUGUI titleText;
 	[SerializeField] private TextMeshProUGUI descriptionText;
 	[SerializeField] private Button playButton;
+	[SerializeField] private Sprite playSprite;
+	[SerializeField] private Sprite pauseSprite;
+	[SerializeField] private Image playButtonImage;
 
 	private AudioClip currentClip;
 	private AudioSource audioSource;
+	private bool isPlaying;
 
 	private void Awake()
 	{
 		audioSource = GetComponent<AudioSource>();
 	}
 
+	private void Update()
+	{
+		if (isPlaying && !audioSource.isPlaying)
+		{
+			isPlaying = false;
+			playButtonImage.sprite = playSprite;
+		}
+	}
 	public void ShowDetail(ItemData item)
 	{
 		titleText.text = item.itemName;
@@ -23,16 +35,39 @@ public class ClueDetailUI : MonoBehaviour
 
 		currentClip = item.audioClip;
 
+		audioSource.Stop();
+		audioSource.clip = null;
+		isPlaying = false;
+		playButtonImage.sprite = playSprite;
+
 		playButton.onClick.RemoveAllListeners();
-		playButton.onClick.AddListener(PlayAudio);
+		playButton.onClick.AddListener(ToggleAudio);
 	}
 
-	private void PlayAudio()
+	private void ToggleAudio()
 	{
 		if (currentClip == null) return;
 
+		if (!isPlaying)
+		{
+			audioSource.clip = currentClip;
+			audioSource.Play();
+			isPlaying = true;
+			playButtonImage.sprite = pauseSprite;
+		}
+		else
+		{
+			audioSource.Pause();
+			isPlaying = false;
+			playButtonImage.sprite = playSprite;
+		}
+	}
+
+	public void HideDetail()
+	{
 		audioSource.Stop();
-		audioSource.clip = currentClip;
-		audioSource.Play();
+		audioSource.clip = null;
+		isPlaying = false;
+		playButtonImage.sprite = playSprite;
 	}
 }
