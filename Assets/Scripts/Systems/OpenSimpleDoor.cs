724e06487bbc04832aef2646008eeabbf78dccf2
Melodeitea <118695764+Melodeitea@users.noreply.github.com>
2026-05-10 17:35:46 +0200

    fixed door animations
    also regular door opening

---
+++ Assets/Scripts/Systems/OpenSimpleDoor.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class OpenSimpleDoor : MonoBehaviour, IInteractable
+{
+	[SerializeField] private string doorID;
+	[SerializeField] private string prompt = "Open door";
+	[SerializeField] private Animator doorAnimator;
+	[SerializeField] private UnityEngine.Events.UnityEvent onOpened;
+
+	private bool _opened = false;
+
+	private void Start()
+	{
+		if (GameFlags.Instance != null && GameFlags.Instance.GetFlag("opened_" + doorID))
+		{
+			_opened = true;
+			if (doorAnimator != null)
+			{
+				doorAnimator.enabled = true;
+				doorAnimator.Play(doorAnimator.GetCurrentAnimatorStateInfo(0).fullPathHash, 0, 1f);
+			}
+		}
+	}
+
+	public string GetPrompt()
+	{
+		return _opened ? "" : $"[E] {prompt}";
+	}
+
+	public void Interact(Player player)
+	{
+		if (_opened) return;
+
+		_opened = true;
+		GameFlags.Instance?.SetFlag("opened_" + doorID);
+
+		if (doorAnimator != null)
+			doorAnimator.enabled = true;
+		else
+			Debug.LogWarning($"OpenSimpleDoor '{doorID}': no Animator assigned.");
+
+		onOpened?.Invoke();
+	}
+}
